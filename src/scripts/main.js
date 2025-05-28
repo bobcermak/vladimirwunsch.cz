6203f83966e69d6787b147824288d9e0bffd735e
bobcermak <bohuslavcermak62@gmail.com>
2025-05-28 11:39:17 +0200

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+import './fontawesome.js';
+import './animations.js';
+import './swiper.js';
+import './hamburger.js';
+
+//footer
+document.getElementById("year").textContent = new Date().getFullYear();
---
+++ src/scripts/swiper.js
@@ -0,0 +1,24 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+//CSS
+import '/src/styles/swiper.css';
+
+//Logic
+// const swiper = new Swiper('.swiper', {
+//     modules: [Navigation, Pagination, Autoplay],
+//     slidesPerView: 1,
+//     spaceBetween: 40,
+//     loop: true,
+//     pagination: {
+//         clickable: true,
+//         el: '.swiper-pagination',
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     autoplay: {
+//         delay: 12000,
+//         disableOnInteraction: true,
+//     }
+// });
