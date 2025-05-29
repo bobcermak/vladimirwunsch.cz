5886cabc021cefcfbfeee29ce558bf3ebf7a7191
bobcermak <bohuslavcermak62@gmail.com>
2025-05-29 14:39:34 +0200

    Updates

--- assets/main-DdWmZq8P.js
+++
@@ -1,1 +0,0 @@
-import"./fontawesome-BW6TkP4k.js";import"./animations-3FWGSTRN.js";import"./swiper-Z3u_fkm7.js";/* empty css               */document.getElementById("year").textContent=new Date().getFullYear();
