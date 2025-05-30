f4a8d4ff936b4dc85d04e4547b90a5594b5c0f4c
bobcermak <bohuslavcermak62@gmail.com>
2025-05-30 14:46:54 +0200

    Implement structural updates and optimizations across multiple modules

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -3,22 +3,26 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 //CSS
 import '/src/styles/swiper.css';
 
-//Logic
-// const swiper = new Swiper('.swiper', {
-//     modules: [Navigation, Pagination, Autoplay],
-//     slidesPerView: 1,
-//     spaceBetween: 40,
-//     loop: true,
-//     pagination: {
-//         clickable: true,
-//         el: '.swiper-pagination',
-//     },
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-//     autoplay: {
-//         delay: 12000,
-//         disableOnInteraction: true,
-//     }
-// });
+//page__about-me - swiper
+const swiper = new Swiper('.page__about-me-stats-swiper', {
+    modules: [Navigation, Pagination, Autoplay],
+    slidesPerView: 1,
+    spaceBetween: 24,
+    loop: true,
+    pagination: {
+        clickable: true,
+        el: '.page__about-me-stats-swiper-pagination',
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true,
+    },
+    breakpoints: {
+        641: {
+            slidesPerView: 2
+        },
+        961: {
+            slidesPerView: 3
+        }
+    }
+})
