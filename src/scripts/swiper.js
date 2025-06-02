0a9f818ab0de8c14a4772baea8430f44dbb64f2e
bobcermak <bohuslavcermak62@gmail.com>
2025-06-02 11:56:09 +0200

    Refactor: Update swiper functionality and styles for improved layout and responsiveness

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -4,14 +4,14 @@ import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import '/src/styles/swiper.css';
 
 //page__about-me - swiper
-const swiper = new Swiper('.page__about-me-stats-swiper', {
+const swiperAboutMe = new Swiper('.page__about-me-stats-content', {
     modules: [Navigation, Pagination, Autoplay],
     slidesPerView: 1,
     spaceBetween: 24,
     loop: true,
     pagination: {
         clickable: true,
-        el: '.page__about-me-stats-swiper-pagination',
+        el: '.page__about-me-stats-content-swiper-pagination',
     },
     autoplay: {
         delay: 5000,
@@ -22,6 +22,30 @@ const swiper = new Swiper('.page__about-me-stats-swiper', {
             slidesPerView: 2
         },
         961: {
+            slidesPerView: 3
+        }
+    }
+})
+
+//page__my-companies - swiper
+const swiperMyCompanies = new Swiper('.page__my-companies-content', {
+    modules: [Navigation, Pagination, Autoplay],
+    slidesPerView: 1,
+    spaceBetween: 24,
+    loop: true,
+    pagination: {
+        clickable: true,
+        el: '.page__my-companies-content-swiper-pagination',
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true,
+    },
+    breakpoints: {
+        641: {
+            slidesPerView: 2
+        },
+        1281: {
             slidesPerView: 3
         }
     }
