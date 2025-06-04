1d143bc71c5cb8183cfe9c3efffcf024c9825ff9
bobcermak <bohuslavcermak62@gmail.com>
2025-06-04 10:09:01 +0200

    Refactor: Remove advertisement close button functionality and adjust related styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,25 +2,12 @@ import './fontawesome.js';
 import './animations.js';
 import './swiper.js';
 
-//page__advertisement - close button
+//page__advertisement - none content
 document.addEventListener('DOMContentLoaded', () => {
-  const closeBtn = document.getElementById('close-advertisement');
   const adv = document.querySelector('.page__advertisement');
-  if (closeBtn && adv) {
-    closeBtn.addEventListener('click', () => {
-      gsap.to(adv, {
-        height: 0,
-        opacity: 0,
-        marginBottom: 0,
-        paddingTop: 0,
-        paddingBottom: 0,
-        duration: 0.5,
-        ease: "power2.inOut",
-        onComplete: () => {
-          adv.style.display = 'none';
-        }
-      });
-    });
+  const advText = adv?.querySelector('.page__advertisement-text');
+  if (adv && advText && advText.textContent.trim() === "") {
+    adv.style.display = 'none';
   }
 });
 
