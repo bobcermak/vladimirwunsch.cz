dd9eb9f20d43b9d6c149960e2cc9639861cafc9b
bobcermak <bohuslavcermak62@gmail.com>
2026-08-18 15:00:46 +0200

    hotovo!!!

--- src/scripts/tracking.js
+++ src/scripts/tracking.js
@@ -1,4 +1,4 @@
-import Clarity from '@microsoft/clarity';
+// import Clarity from '@microsoft/clarity';
 
-const projectId = "tcvav4k8k6"
-Clarity.init(projectId);
+// const projectId = "tcvav4k8k6"
+// Clarity.init(projectId);
